9036d762910f34290fdefbba4024e3b60dc03ea1
wisang <wseom7@gmail.com>
2022-10-25 18:47:41 +0900

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { useState } from "react";
 
-const items = [];
-
 function App() {
   const [item, setItem] = useState("");
+  const [items, setItems] = useState([]);
 
   function handleChange(event) {
     setItem(event.target.value);
   }
 
   function addItem() {
-    items.push(item);
+    setItems((prevItems) => {
+      return [...prevItems, item];
+    });
+
     setItem("");
-    console.log(items);
   }
 
   return (
@@ -22,7 +23,7 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" />
+        <input onChange={handleChange} type="text" value={item} />
         <button onClick={addItem}>
           <span>Add</span>
         </button>
