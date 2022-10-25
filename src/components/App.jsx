d93bd44c2b144dfb06520e3233e4e96a25f825bf
wisang <wseom7@gmail.com>
2022-10-25 18:33:13 +0900

    working in my own way

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
+import { useState } from "react";
+
+const items = [];
 
 function App() {
+  const [item, setItem] = useState("");
+
+  function handleChange(event) {
+    setItem(event.target.value);
+  }
+
+  function addItem() {
+    items.push(item);
+    setItem("");
+    console.log(items);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" />
+        <button onClick={addItem}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {items.map((todoItem) => (
+            <li>{todoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
